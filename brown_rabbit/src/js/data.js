14e51adcf09ab6657e0b4757689f2310968a4aa1
Jemima Masamu <mimamenser@yahoo.com>
2023-07-04 02:22:49 +0200

    Update data.js

--- brown_rabbit/src/js/data.js
+++ brown_rabbit/src/js/data.js
@@ -1,1 +1,106 @@
+const coffeeItems = [
+    {
+        title: 'Wonderful Copenhagen 2021',
+        imageSrc: '../src/assets/images/posts/Post thumbnail-4.png',
+        altText: 'Coffee cups'
+    },
+    {
+        title: 'The most expensive coffee on the market',
+        imageSrc: '../src/assets/images/posts/Post thumbnail-1.png',
+        altText: 'A hand holding hot coffee cup'
+    },
+    {
+        title: '10 type of coffee beans you need to know',
+        imageSrc: '../src/assets/images/posts/Post thumbnail-2.png',
+        altText: 'Coffee beans'
+    },
+    {
+        title: 'Challenge your barista skills to the max',
+        imageSrc: '../src/assets/images/posts/Post thumbnail-3.png',
+        altText: 'Coffee mugs'
+    }
+];
+
+
+const logos = [
+    {
+        url: '../src/assets/images/sponsors/sponsor-20.png',
+        altText: 'MoJo ToGo' 
+    }, 
+    {
+        url: '../src/assets/images/sponsors/sponsor-1.png',
+        altText: 'banaqua' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-2.png',
+        altText: 'CASK Norway' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-3.png',
+        altText: 'NATURAL MILK' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-4.png',
+        altText: 'TROLD' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-5.png',
+        altText: 'FIGGJO' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-6.png',
+        altText: 'ESPRESSO SUPPLY' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-7.png',
+        altText: 'EGGE GÅRD' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-8.png',
+        altText: 'ESPRESSOPARTS' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-9.png',
+        altText: 'O' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-10.png',
+        altText: 'MERCANTA' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-11.png',
+        altText: 'MAHLKÖNIG' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-12.png',
+        altText: 'Asociación Colombiana de Cafés Especiales' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-13.png',
+        altText: 'LINCOLIN & YORK' 
+    },{
+        url: '../src/assets/images/sponsors/sponsor-14.png',
+        altText: 'DAVINCI' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-15.png',
+        altText: 'BUNN' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-16.png',
+        altText: 'MOCCAMASTER' 
+    },{
+        url: '../src/assets/images/sponsors/sponsor-17.png',
+        altText: 'Café de Colombia' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-18.png',
+        altText: 'Cafetto' 
+    },
+    {
+        url: '../src/assets/images/sponsors/sponsor-19.png',
+        altText: 'Smart Roast' 
+    }
+];
+
 
