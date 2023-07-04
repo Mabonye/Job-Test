2648c57fa29d1870f4fd54ad1bceb5eec0511985
Jemima Masamu <mimamenser@yahoo.com>
2023-07-04 02:21:50 +0200

    Update scripts.js

--- brown_rabbit/src/js/scripts.js
+++ brown_rabbit/src/js/scripts.js
@@ -1,14 +1,45 @@
-window.onload = function () {
+const coffeeContainer = document.querySelector('.posts');
+const logoList = document.querySelector('#logos');
 
-    var el = document.createElement("div");
-    el.innerHTML = "RABBIT TEST ";
-    var div = document.getElementById("headline");
-    insertAfter(div, el);
-
-    /* FUNCTIONS */
-
-    // Inserts newNode after referenceNode
-    function insertAfter(referenceNode, newNode) {
-        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+function loadCoffeeItems() {
+    for (let i = 0; i < coffeeItems.length; i++) {
+        coffeeContainer.innerHTML += `
+            <a href="#" class="item">
+                <div>
+                    <img src="${coffeeItems[i].imageSrc}" alt="${coffeeItems[i].altText}">
+                </div>
+                <div class="item-desc">
+                    <h2>${coffeeItems[i].title}</h2>
+                    <time datetime="2021-12-07">Published <span>12/07/2021</span></time>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat...</p>
+                    <button>Read more</button>
+                </div>
+            </a>
+        `
     }
-};
+
+    coffeeContainer.innerHTML += `
+        <hr>
+    
+        <div class="buttons">
+            <button>Previous</button>
+            <button>Next</button>
+        </div>
+    `;
+}
+
+function loadSponsorImages() {
+    logos.forEach((logo) => {
+        logoList.innerHTML += `
+            <img src="${logo.url}" alt="${logo.altText}" />
+        `
+    })
+}
+
+
+window.addEventListener('load', function () {
+    loadCoffeeItems();
+    loadSponsorImages();
+});
